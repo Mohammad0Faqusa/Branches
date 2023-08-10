8b59ccccbc47b7626489d3b81028d48a26a83cdb
Mohammad Faqusa <201014@ppu.edu.ps>
2023-08-10 18:33:26 +0300

    after stash

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+<<<<<<< Updated upstream
 console.log('hello f2 !'); 
+=======
+console.log('hello f222 !');
+console.log('hello f1 !');
+console.log('what about diff ? ');
+console.log('what about git diff head ?'); 
+>>>>>>> Stashed changes
