4f757366d93040909ed12a5afc5ef6253f1a20b0
Mohammad Faqusa <201014@ppu.edu.ps>
2023-08-10 18:35:23 +0300

    last commit f2

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+<<<<<<< Updated upstream
 console.log('hello f2 !');
 console.log('hello f1 !');
 console.log('what about diff ? ');
 console.log('what about git diff head ?'); 
+=======
+
+console.log('hello f2 !');
+
+console.log('hello f222 !');
+console.log('hello f1 !');
+console.log('what about diff ? ');
+console.log('what about git diff head ?');
+
+>>>>>>> Stashed changes
